57e3d2b7cbf4623263cec95319e17c3aac37a68a
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-07-02 19:04:35 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-""" method that determines if all the boxes can be opened """
+"""method that determines if all the boxes can be opened"""
+
 
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
-    
-    # Set to keep track of opened boxes
-    opened = set()
-    
-    # Stack for DFS
-    stack = [0]
-    
-    while stack:
-        current_box = stack.pop()
-        
-        if current_box not in opened:
-            opened.add(current_box)
-            for key in boxes[current_box]:
-                if key < len(boxes):
-                    stack.append(key)
-    
-    # Check if all boxes are opened
-    return len(opened) == len(boxes)
+    """function can unlock all boxes"""
+
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
