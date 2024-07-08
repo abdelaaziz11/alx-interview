a203b0d678ea9f703ddcda191902766160151edc
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-07-08 12:23:48 +0100

    minoperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+calculates the fewest number of operations needed to result
+in exactly n H characters in the file
+"""
+
+def minOperations(n):
+    """
+    Returns an integer and If n is impossible to achieve,
+    return 0
+    """
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
+
