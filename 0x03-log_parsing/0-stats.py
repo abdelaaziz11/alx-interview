60c33cb3f6c1995e74a423476f4a5e4fd47fb57b
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-07-15 12:55:32 +0100

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
+
 import sys
 import signal
 
-# Initialize metrics
+''' Initialize metrics '''
 total_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
